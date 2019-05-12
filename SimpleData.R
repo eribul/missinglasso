7dc864444c31571940c2ee144852cb4bbef6821c
DebbieL <debbielau.hk@gmail.com>
2019-05-12 15:09:22 +0200

    New graphs

--- SimpleData.R
+++ SimpleData.R
@@ -1,109 +1,121 @@
 #----setup-------------------------------------------------------------
 
+# clean memory
 rm(list = ls())
 
-
-library(glmnet)
+# load packages
+library(MASS)
+library(Matrix)
+library(lattice)
+library(igraph)
 library(flare)
 
+library(foreach)
+library(glmnet)
+library(tibble)
+
+library(tidyr)
 
 
-#--------dantzig examnple-------------------------------------------------
+#----generate data 1-----------------------------------------------------
+# read the table with nested simulated dataframes
+df_imp <- readRDS("C:/Users/Kokchun/Desktop/temp/df_imp.rds")
 
-set.seed(123)
-n = 100
-d = 200
-d1 = 10
-rho0 = 0.3
-lambda = c(3:1)*sqrt(log(d)/n)
-Sigma = matrix(0,nrow=d,ncol=d)
-Sigma[1:d1,1:d1] = rho0
-diag(Sigma) = 1
-mu = rep(0,d)
-X = mvrnorm(n=2*n,mu=mu,Sigma=Sigma)
-X.fit = X[1:n,]
-X.pred = X[(n+1):(2*n),]
+# extract data dataframes
+# pick p=50, n=1000
+# data1 is zero missing proportion
+# data2 is 0.3 mp & mean imputed
+# data3 is 0.3 mp & median imputed
+data1 <- unnest(df_imp[50,5])
+data2 <- unnest(df_imp[55,5])
+data3 <- unnest(df_imp[56,5])
 
-
-eps = rt(n=n,df=n-1)
-beta = c(rep(sqrt(1/3),3),rep(0,d-3))
-Y.fit = X.fit%*%beta+eps
-## Regression with "dantzig".
-out=slim(X=X.fit,Y=Y.fit,lambda=lambda,method = "lq",q=1)
-
-#------------------------------------------------
-
-
-#----generate data-----------------------------------------------------
-n = 50
-d = 8
-X = matrix(rnorm(n * d), n, d)
-beta = c(3, 2, 0, 1.5, rep(0, d - 4))
-eps = rnorm(n)
-Y = X %*% beta + eps
+# convert dataframes to matrices, suitable as inputs of models
+# for data1
+x1 <- model.matrix(Y~., data1)[,-1]
+y1 <- data1$Y
+# for data2
+x2 <- model.matrix(Y~., data2)[,-1]
+y2 <- data2$Y
+# for data3
+x3 <- model.matrix(Y~., data3)[,-1]
+y3 <- data3$Y
 
 
-# dataframe for complete data
-data1 <- cbind(X,Y) %>%
-  as_tibble()
-colnames(data1)<- c("X1", "X2", "X3", "X4", "X5", "X6", "X7", "X8", "Y")
+#----generate data 2---------------------------------------------------------
+# # Alternatively, generate own data
+# n = 50
+# d = 100
+# X = matrix(rnorm(n * d), n, d)
+# beta = c(3,2,0,1.5,rep(0,d-4))
+# eps = rnorm(n)
+# Y = X %*% beta + eps
+# 
+# # dataframe for complete data
+# data1 <- cbind(X,Y) %>%
+#   as_tibble()
+# #colnames(data1)<- c("X1", "X2", "X3", "X4", "X5", "X6", "X7", "X8", "Y")
+# 
+# x4 <- X
+# y4 <- Y
+# 
+# # dataframe for missing data
+# X[sample.int(nrow(X), floor(nrow(X) * .3)), sample.int(ncol(X), floor(ncol(X) * .3))] <- NA
+# 
+# data2 <- cbind(X,Y) %>%
+#   as_tibble()
+# #colnames(data2)<- c("X1", "X2", "X3", "X4", "X5", "X6", "X7", "X8", "Y")
+# 
+# data2 <- na.omit(data2)
+# 
+# x5 <- model.matrix(V21~., data2)[,-1]
+# y5 <- data2$V21
+# 
+# # imputed with mean
+# X[sample.int(nrow(X), floor(nrow(X) * .2)), sample.int(ncol(X), floor(ncol(X) * .3))] <- NA
+# 
+# data2 <- cbind(X,Y) %>%
+#   as_tibble()
+# colnames(data2)<- c("X1", "X2", "X3", "X4", "X5", "X6", "X7", "X8", "Y")
+# 
+# data2$V1[is.na(data2$V1)] <- mean(data2$V1, na.rm = TRUE)
+# data2$V6[is.na(data2$V6)] <- mean(data2$V6, na.rm = TRUE)
+# 
+# x <- model.matrix(Y~., data2)[,-1]
+# y <- data2$V9
 
-x <- model.matrix(Y~., data1)[,-1]
-y <- data1$Y
 
-
-# dataframe for missing data
-X[sample.int(nrow(X), floor(nrow(X) * .2)), sample.int(ncol(X), floor(ncol(X) * .3))] <- NA
-
-data2 <- cbind(X,Y) %>%
-  as_tibble()
-colnames(data2)<- c("X1", "X2", "X3", "X4", "X5", "X6", "X7", "X8", "Y")
-
-data2 <- na.omit(data2)
-
-x <- model.matrix(Y~., data2)[,-1]
-y <- data2$Y
-
-
-#----Imputation--------------------------------------------------------
-# generate imputed dataframe
-# with mean
-X[sample.int(nrow(X), floor(nrow(X) * .2)), sample.int(ncol(X), floor(ncol(X) * .3))] <- NA
-
-data2 <- cbind(X,Y) %>%
-  as_tibble()
-colnames(data2)<- c("X1", "X2", "X3", "X4", "X5", "X6", "X7", "X8", "Y")
-
-data2$V1[is.na(data2$V1)] <- mean(data2$V1, na.rm = TRUE)
-data2$V6[is.na(data2$V6)] <- mean(data2$V6, na.rm = TRUE)
-
-
-x <- model.matrix(Y~., data2)[,-1]
-y <- data2$V9
-
-#----lasso plot--------------------------------------------------------
-
+#----lasso plot with glmnet--------------------------------------------------------
 # estimate lasso
-lambda <- 10^seq(10, -2, length = 100)
 set.seed(123)
-lasso=glmnet(x,y, alpha = 1, family = "gaussian")
+lasso1 = glmnet(x3, y3, alpha = 1, family = "gaussian")
 
 # plot coefficient path
-plot(lasso)
+plot(lasso1, xvar = "lambda", label = TRUE)
 
 # plot MSE with different lambda
-cvlassofit<-cv.glmnet(x,y, alpha = 1, family = "gaussian" )
+cvlassofit <- cv.glmnet(x1,y1, alpha = 1, family = "gaussian")
 plot(cvlassofit)
 
 # Add line to indicate best lambda
 lambda_min <- cvlassofit$lambda.min
 estimates <- as.vector(coef(lasso, s = lambda_min, exact = TRUE))
 norm. <- sum(abs(estimates))
-plot(lasso, xlim = range(0, norm., as.vector(lasso$beta)))
+plot(lasso, xlim = range(0, norm., as.vector(lasso$beta)), xvar = "lambda", label = TRUE)
 abline(v = norm., col = "red")
 
 
+#----Lasso plot with flare------------------------------------------
+nlamb = 500
+ratio = 0.3
 
+lasso2 = slim(X=x3,Y=y3,nlambda=nlamb,lambda.min.ratio=ratio,method="lasso")
+matplot(log(lasso2$lambda), t(lasso2$beta), type = "l", main = "Regularization Path", 
+        xlab = "log(lambda)", ylab = "Coefficient")
 
-
-
+#----Dantzig plot with flare----------------------------------------
+dz = slim(X=x1,Y=y1,nlambda=nlamb,lambda.min.ratio=ratio,method="dantzig")
+#plot(dz)
+matplot(log(dz$lambda), t(dz$beta), type = "l", main = "Regularization Path", 
+        xlab = "log(lambda)", ylab = "Coefficient")
+plot(dz)
